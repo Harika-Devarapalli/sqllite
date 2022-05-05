e70344dff48e84db6fbd6d64f980c83e5a6fbf79
Harika-Devarapalli <devarapalliharika01@gmail.com>
2022-05-05 18:54:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sqllite/DBHelper.java
@@ -0,0 +1,23 @@
+package com.example.sqllite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "Userdata.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+
+    }
+}
